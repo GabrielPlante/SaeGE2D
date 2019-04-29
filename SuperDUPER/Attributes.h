b84087cb45cf01ed2ae714d587c93e4737e9a3c1
kali cops <gabriel.plante@live.fr>
2019-04-29 21:40:51 +0200

    Changed attributes from class to struct

--- SuperDUPER/Attributes.h
+++ SuperDUPER/Attributes.h
@@ -1,10 +1,12 @@
 #pragma once
-class Attributes
+struct Attributes
 {
 public:
-	Attributes();
-	~Attributes();
-private:
+	Attributes(int moveSpeed = 0, int meleeCombat = 0, int rangeCombat = 0, int strenght = 0, int stamina = 0, int agility = 0, int mentalStrenght = 0,
+		int craftsmanship = 0, int intellect = 0, int cunning = 0, int sociability = 0)
+		:moveSpeed{ moveSpeed }, meleeCombat{ meleeCombat }, rangeCombat{ rangeCombat }, strenght{ strenght }, stamina{ stamina }, agility{ agility },
+		mentalStrenght{ mentalStrenght }, craftsmanship{ craftsmanship }, intellect{ intellect }, cunning{ cunning }, sociability{ sociability }
+	{}
 	int moveSpeed;
 	int meleeCombat;
 	int rangeCombat;
